92df40365e228abf8b7289b224442f43771813a0
fluffypancakes <cdiep@stuy.edu>
2018-04-09 19:14:03 -0400

    added the rest of the necessary methods with exceptions

--- 08Clap/MyLinkedList.java
+++ 08Clap/MyLinkedList.java
@@ -8,8 +8,16 @@ public class MyLinkedList {
 		MyLinkedList A = new MyLinkedList ( );
 		A.add( 5 );
 		A.add( 7 );
-		System.out.println ( A );
-		// System.out.println ( A.get( 1 ) );
+		System.out.println( A );
+		System.out.println( A.get( 1 ) );
+		// A.clear( );
+		A.set( 1 , 6 );
+		A.add( 1 , 4 );
+		System.out.println( A );
+		// A.remove( Integer.valueOf( 4 ) );
+		A.remove( 1 );
+		System.out.println( A );
+		// System.out.println ( A.indexOf( 6 ) );
 	}
 
 	public MyLinkedList ( ) {
@@ -18,10 +26,36 @@ public class MyLinkedList {
 		size = 0;
 	}
 
+	public void clear ( ) {
+		start = null;
+		end = null;
+		size = 0;
+	}
+
+	public int size ( ) {
+		return this.size;
+	}
+
+	private Node getNode ( int index ) {
+		int currentIndex = 0;
+		Node current = start;
+		while ( !(current == null) ) {
+			if ( currentIndex == index ) {
+				return current;
+			}
+			currentIndex++;
+			current = current.getNext( );
+		}
+		return null;
+	}
+
 	public String toString ( ) {
 		String out = "[";
 		Node current = start;
 		int currentIndex = 0;
+		if ( size == 0 ) {
+			return out + " ]";
+		}
 		while ( !(current == null) && currentIndex < size ) {
 			out += current.getValue( );
 			if ( !current.hasNext( ) ) {
@@ -36,9 +70,9 @@ public class MyLinkedList {
 		return out;
 	}
 
-	public boolean add ( Integer newData ) {
-		// System.out.println( newData );
-		Node newNode = new Node ( newData ); 
+	public boolean add ( Integer data ) {
+		// System.out.println( data );
+		Node newNode = new Node ( data ); 
 		if ( size == 0 ) {
 			start = newNode;
 			end = newNode;
@@ -53,9 +87,10 @@ public class MyLinkedList {
 	}
 
 	public Integer get ( int index ) {
-		if ( index >= size ) {
+		if ( index >= size || index < 0) {
 			throw new IndexOutOfBoundsException ( "array index out of bounds :(" );
 		}
+		/* 
 		int currentIndex = 0;
 		Node current = start;
 		while ( !(current == null) && currentIndex < size ) {
@@ -66,10 +101,92 @@ public class MyLinkedList {
 			currentIndex++;
 		}
 		return -1;
+		*/
+		return getNode( index ).getValue( );
 	}
 
 	public Integer set ( int index , Integer data ) {
-		return 0;
+		if ( index >= size || index < 0 ) {
+			throw new IndexOutOfBoundsException ( "array index out of bounds :(" );
+		}
+		getNode( index ).setValue( data );
+		return data;
+	}
+
+	public int indexOf ( Integer data ) {
+		int currentIndex = 0;
+		Node current = start;
+		while ( !(current == null) && currentIndex < size ) {
+			if ( data.equals( current.getValue( ) ) ) {
+				return currentIndex;
+			}
+			current = current.getNext( );
+			currentIndex++;
+		}
+		return -1;
+	}
+
+	public void add ( int index , Integer data ) {
+		if ( index == size ) {
+			add ( data );
+		}
+		Node newNode = new Node ( data );
+		if ( index == 0 ) {
+			start.setPrev( newNode );
+			newNode.setNext( start );
+			start = newNode;
+		}
+		else { 
+			Node neighbor = getNode ( index );
+			newNode.setNext( neighbor );
+			newNode.setPrev( neighbor.getPrev( ) );
+			neighbor.getPrev( ).setNext( newNode );
+			neighbor.setPrev( newNode );
+		}
+		size++;
+	}
+
+	public boolean remove ( Integer data ) {
+		int index = indexOf ( data );
+		if ( index == -1 ) {
+			return false;
+		}
+		Node target = getNode ( indexOf ( data ) );
+		if ( index == 0 ) {
+			start = target.getNext( );
+			target.getNext( ).setPrev( null );
+		}
+		else if ( index == size - 1 ) {
+			end = target.getPrev( );
+			target.getPrev( ).setNext( null );
+		}
+		else {
+			target.getPrev( ).setNext( target.getNext( ) );
+			target.getNext( ).setPrev( target.getPrev( ) ); 
+		}
+		size--;
+		return true;
+	}
+
+	public Integer remove ( int index ) {
+		if ( index >= size || index < 0 ) {
+			throw new IndexOutOfBoundsException ( "array index out of bounds :(" );
+		}
+		Node target = getNode ( index );
+		if ( index == 0 ) {
+			start = target.getNext( );
+			target.getNext( ).setPrev( null );
+		}
+		else if ( index == size - 1 ) {
+			end = target.getPrev( );
+			target.getPrev( ).setNext( null );
+		}
+		else {
+			target.getPrev( ).setNext( target.getNext( ) );
+			target.getNext( ).setPrev( target.getPrev( ) ); 
+		}
+		size--;
+		return target.getValue( );
 	}
 
 	private class Node {
@@ -110,7 +227,7 @@ public class MyLinkedList {
 			return next;
 		}
 
-		public int getValue ( ) {
+		public Integer getValue ( ) {
 			return data;
 		}
 
