99e8011f4b4cafc0228de9e76685b59abf77d34e
fluffypancakes <cdiep@stuy.edu>
2018-04-08 22:26:53 -0400

    added add and get

--- 08Clap/MyLinkedList.java
+++ 08Clap/MyLinkedList.java
@@ -1,4 +1,4 @@
-public class MyLinkedList implements List {
+public class MyLinkedList {
 	
 	private int size;
 	private Node start;
@@ -7,7 +7,9 @@ public class MyLinkedList implements List {
 	public static void main(String[] args) {
 		MyLinkedList A = new MyLinkedList ( );
 		A.add( 5 );
+		A.add( 7 );
 		System.out.println ( A );
+		// System.out.println ( A.get( 1 ) );
 	}
 
 	public MyLinkedList ( ) {
@@ -19,7 +21,8 @@ public class MyLinkedList implements List {
 	public String toString ( ) {
 		String out = "[";
 		Node current = start;
-		while ( !current.equals( null ) ) {
+		int currentIndex = 0;
+		while ( !(current == null) && currentIndex < size ) {
 			out += current.getValue( );
 			if ( !current.hasNext( ) ) {
 				out += "]";
@@ -28,22 +31,25 @@ public class MyLinkedList implements List {
 				out += ", ";
 			}
 			current = current.getNext( );
+			currentIndex++;
 		}
 		return out;
 	}
 
 	public boolean add ( Integer newData ) {
+		// System.out.println( newData );
 		Node newNode = new Node ( newData ); 
 		if ( size == 0 ) {
 			start = newNode;
 			end = newNode;
-
 		}
 		else { 
+			end.setNext( newNode );
 			newNode.setPrev( end );
 			end = newNode;
 		}
 		size++;
+		return true;
 	}
 
 	public Integer get ( int index ) {
@@ -51,17 +57,71 @@ public class MyLinkedList implements List {
 			throw new IndexOutOfBoundsException ( "array index out of bounds :(" );
 		}
 		int currentIndex = 0;
-		for ( Node current : this ) {
-			if ( currentIndex == index ) {
+		Node current = start;
+		while ( !(current == null) && currentIndex < size ) {
+			if ( index == currentIndex ) {
 				return current.getValue( );
 			}
+			current = current.getNext( );
 			currentIndex++;
 		}
 		return -1;
 	}
 
 	public Integer set ( int index , Integer data ) {
-
+		return 0;
 	}
+
+	private class Node {
+	
+		private Node prev;
+		private Node next;
+		private Integer data;	
+
+		public Node ( Integer data ) {
+			prev = null;
+			next = null;
+			this.data = data;
+		}	
+
+		public Node ( Node prev , Node next , Integer data) {
+			this.prev = prev;
+			this.next = next;
+			this.data = data;
+		}	
+
+		private void setPrev ( Node prev ) {
+			this.prev = prev; 
+		}
+
+		private void setNext ( Node next ) {
+			this.next = next;
+		}
+
+		private void setValue ( Integer value ) {
+			data = value;
+		}
+
+		public Node getPrev ( ) {
+			return prev;
+		}
+
+		public Node getNext ( ) {
+			return next;
+		}
+
+		public int getValue ( ) {
+			return data;
+		}
+
+		public boolean hasNext ( ) {
+			return !(this.getNext( ) == null);
+		}
+
+		public String toString ( ) {
+			return "" + data;
+		}
+
+	}	
 
 }
