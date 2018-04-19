f2a77a1b8a8e55bc6456f210e15ccf09cbacf260
fluffypancakes159 <cdiep@stuy.edu>
2018-04-19 14:50:40 -0400

    working add stuff

--- 10Hands/MyDeque.java
+++ 10Hands/MyDeque.java
@@ -1,13 +1,18 @@
 import java.util.*;
 
-public class MyDeque<E> implements Queue<E> {
+public class MyDeque<E> /*implements Queue<E>*/ {
 	
 	private E[] data;
 	private int start;
 	private int end;
+	private int len;
 
 	public static void main(String[] args) {
-		
+		MyDeque<Integer> A = new MyDeque<>( );
+		A.addLast( 4 );
+		A.addFirst( 5 );
+		A.addLast( 4 );
+		System.out.println( A );
 	}
 
 	@SuppressWarnings ( "unchecked" )
@@ -15,10 +20,83 @@ public class MyDeque<E> implements Queue<E> {
 		data = (E[]) new Object[10];
 		start = 4;
 		end = 4;
+		len = 0;
 	}
 
-	public E peek ( ) {
-		
+	public MyDeque ( int size ) {
+		if ( size >= 0 ) {
+			throw new IllegalArgumentException( "insufficient size :(" );
+		}
+		data = (E[]) new Object[size];
+		start = size / 2;
+		end = size / 2;
+	}
+
+	public int size ( ) {
+		return len;
+	}
+
+	public String toString ( ) {
+		int current = start;
+		String out = "[";
+		System.out.println( current % data.length );
+		System.out.println( data[current % data.length + 1] );
+		if ( len == 1 ) {
+			return out + data[current] + "]";
+		}
+		while ( current % data.length - 1 != end ) {
+			if ( current % data.length == end ) {
+				out += data[current % data.length] + "]";
+				break;
+			}
+			else {
+				out += data[current % data.length] + ", ";
+			}
+			current++; 
+		}
+		return out;
+	}
+
+	public void addFirst ( E elem ) {
+		if ( elem == null ) {
+			throw new NullPointerException ( "cannot add null :(" );
+		}
+		if ( len == 0 ) {
+			len++;
+			data[start] = elem;
+			return;
+		}
+		else {
+			if ( start == 0 ) {
+				start = data.length - 1;
+			}
+			else {
+				start--;
+			}
+			data[start] = elem;
+			len++;
+		}
+	}
+
+	public void addLast ( E elem ) {
+		if ( elem == null ) {
+			throw new NullPointerException ( "cannot add null :(" );
+		}
+		if ( len == 0 ) {
+			len++;
+			data[end] = elem;
+			return;
+		}
+		else{
+			if ( end == data.length - 1 ) {
+				end = 0;
+			}
+			else {
+				end++;
+			}
+			data[end] = elem;
+			len++;
+		}
 	}
 
 }
